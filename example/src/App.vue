0ddb003bf2f49fcf7a63da84cc33b457da13e1cc
Wizard67 <wizard67.hjn@gmail.com>
2020-07-10 18:22:32 +0800

    feat(example): add example

---
+++ example/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const text = ref('hello vue3!')
+
+    return {
+      text
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import Button from './components/button.vue'
+import Button from './components/button/index.vue'
 
 export { Button }
