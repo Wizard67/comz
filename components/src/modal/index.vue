9a8cf53fae26590a864c36da129fd2521c9189c2
Wizard67 <wizard67.hjn@gmail.com>
2020-07-25 23:48:13 +0800

    feat(component:modal): enhance component

--- components/src/modal/index.vue
+++ components/src/modal/index.vue
@@ -1,31 +1,57 @@
 <template>
-  <div
-    class="modal"
-    v-if="show"
-    @click.self="handleWrapClick"
-  >
-    <div class="modal__content">
-      <slot></slot>
+  <transition name="modal-fade">
+    <div
+      class="modal"
+      v-if="show"
+      @click.self="handleModalClose"
+    >
+      <div class="modal__content">
+        <Card shadow>
+          <template #header>
+            <slot name="header">
+              {{ title || '提示' }}
+            </slot>
+          </template>
+          <template #default>
+            <slot></slot>
+          </template>
+          <template #footer>
+            <slot name="footer">
+              <Button @on-click="handleModalClose">关闭</Button>
+            </slot>
+          </template>
+        </Card>
+      </div>
     </div>
-  </div>
+  </transition>
+  <transition name="modal-backdrop-fade">
+    <div v-if="show" class="modal-backdrop"></div>
+  </transition>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import {} from 'vue'
+import Card from '../card/index.vue'
+import Button from '../button/index.vue'
 
 declare function emit(event: 'update:show', state: boolean): void
 declare function emit(event: 'on-close'): void
 
 declare const props: {
   show: boolean
+  title?: String
 }
 
-export const handleWrapClick = () => {
-
+export const handleModalClose = () => {
   emit('update:show', false)
   emit('on-close')
 }
 
+export default {
+  components: {
+    Card,
+    Button
+  }
+}
 </script>
 
 <style lang="scss">
@@ -36,16 +62,81 @@ export const handleWrapClick = () => {
   left: 0;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
   z-index: 200;
 
   &__content {
-    width: 200px;
-    height: 300px;
-    background-color: white;
+    flex: none;
+    width: 300px;
+    margin-top: 100px;
+  }
+}
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, .5);
+  transform: translate3d(0, 0, 0);
+}
+
+.modal-fade-enter-active {
+  animation: modal-fade-in .3s;
+}
+
+.modal-fade-leave-active {
+  animation: modal-fade-out .3s;
+}
+
+@keyframes modal-fade-in {
+  0% {
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+  100% {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+
+@keyframes modal-fade-out {
+  0% {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  100% {
+    transform: translate3d(0, -20px, 0);
+    opacity: 0;
+  }
+}
+
+.modal-backdrop-fade-enter-active {
+  animation: modal-backdrop-fade-in .3s;
+}
+
+.modal-backdrop-fade-leave-active {
+  animation: modal-backdrop-fade-out .3s;
+}
+
+@keyframes modal-backdrop-fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes modal-backdrop-fade-out {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
   }
 }
 </style>
