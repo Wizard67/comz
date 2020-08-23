e08ef8df960bfd59f2b5a528bc376f32b4c50834
Wizard67 <wizard67.hjn@gmail.com>
2020-08-24 00:06:06 +0800

    feat: suport css variables scoped

--- packages/vca/src/dom/useCssVars.ts
+++ packages/vca/src/dom/useCssVars.ts
@@ -7,27 +7,39 @@ import {
 
 type Value = number | string | undefined
 
-type TransValueToString = (value: Value) => string
+type TransValueToString = (value: Value, scoped: boolean) => string
 
-type UseCssVars = (vars: { [property: string]: ComputedRef<Value> }) => void
+type UseCssVars = (
+  vars: {
+    [property: string]: ComputedRef<Value>
+  },
+  options?: {
+    scoped?: boolean
+  }
+) => void
 
-const transValueToString: TransValueToString = value => {
+const InvalidValue = 'invalid'
+
+const transValueToString: TransValueToString = (value, scoped) => {
   switch (typeof value) {
     case 'undefined':
-      return ''
+      return scoped ? InvalidValue: ''
     case 'number':
       return String(value)
     case 'string':
-      return value
+      return value || (scoped ? InvalidValue: '')
   }
 }
 
-export const useCssVars: UseCssVars = vars => {
+export const useCssVars: UseCssVars = (vars, options) => {
   const varTrans = reactive({})
 
   for (const property in vars) {
-    varTrans[property] = computed(() => 
-      transValueToString(vars[property].value)
+    varTrans[property] = computed(() =>
+      transValueToString(
+        vars[property].value,
+        options?.scoped || false
+      )
     )
   }
 
