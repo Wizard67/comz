6f073f949a1a263216347f27c97c2429303278b1
Wizard67 <wizard67.hjn@gmail.com>
2020-09-15 23:33:26 +0800

    refactor: update icons

---
+++ packages/icons/src/letter-f.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-letter-f" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z"/>
+    <path d="M17 4h-10v16" />
+    <line x1="7" y1="12" x2="15" y2="12" />
+  </svg>
+</template>
---
+++ packages/icons/src/letter-h.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-letter-h" width="1em" height="1em" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z"/>
+    <line x1="17" y1="4" x2="17" y2="20" />
+    <line x1="7" y1="12" x2="17" y2="12" />
+    <line x1="7" y1="4" x2="7" y2="20" />
+  </svg>
+</template>
