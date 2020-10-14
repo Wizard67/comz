a0cc89094c808ffc63709a3bb39114b6753d8961
Wizard67 <wizard67.hjn@gmail.com>
2020-10-14 16:13:42 +0800

    perf(type): enhance useEvent type

--- packages/vca/src/useEvent/index.ts
+++ packages/vca/src/useEvent/index.ts
@@ -1,16 +1,18 @@
 import { ref, Ref, isRef, watch } from 'vue'
 
-export const useEvent = (
+export const useEvent = <
+  K extends (keyof WindowEventMap | DocumentEventMap)
+>(
   target: EventTarget | Ref<EventTarget>,
-  type: string,
+  type: K,
   listener: EventListenerOrEventListenerObject,
   options?: boolean | AddEventListenerOptions
 ) => {
   const targetRef = isRef(target) ? target : ref(target)
 
   const stopWatch = watch(targetRef, (element, _, cleanUp) => {
-    element.addEventListener(type, listener, options)
-    cleanUp(() => element.removeEventListener(type, listener))
+    element.addEventListener(type as string, listener, options)
+    cleanUp(() => element.removeEventListener(type as string, listener))
   }, { immediate: true })
 
   return () => {
