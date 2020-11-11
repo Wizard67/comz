ec2393c2cd02d7332f66a5b9040afb676809b844
Wizard67 <wizard67.hjn@gmail.com>
2020-11-12 00:23:46 +0800

    refactor(component:modal): refactor component

--- packages/comz/src/modal/modal.vue
+++ packages/comz/src/modal/modal.vue
@@ -1,39 +1,29 @@
 <template>
   <teleport to="body">
-    <transition name="modal-fade">
+    <transition name="modal">
       <div
         v-if="show"
-        class="c-modal"
+        class="cmodal"
         :style="cssVars"
-        @click.self="closable ? handleModalClose() : void 0"
+        @click.self="handleMaskClick"
       >
-        <div class="c-modal__content">
-
+        <div class="cmodal__content">
           <Card shadow>
             <template #header>
-              <slot
-                name="header"
-                :close="handleModalClose"
-              >
+              <slot name="header" :close="handleClose">
                 <template v-if="true">{{ title || '提示' }}</template>
-                <Icon v-if="closable" @on-click="handleModalClose" link><X/></Icon>
+                <Icon v-if="closable" @on-click="handleClose" link><X/></Icon>
               </slot>
             </template>
-
             <template #default>
-              <slot></slot>
+              <slot />
             </template>
-
             <template #footer>
-              <slot
-                name="footer"
-                :close="handleModalClose"
-              >
-                <Button @on-click="handleModalClose">关闭</Button>
+              <slot name="footer" :close="handleClose">
+                <Button @on-click="handleClose">关闭</Button>
               </slot>
             </template>
           </Card>
-
         </div>
       </div>
     </transition>
@@ -41,18 +31,6 @@
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed } from 'vue'
-import { useCssVars } from '@comz/vca'
-
-import Card from '../card/card.vue'
-import Button from '../button/button.vue'
-import Icon from '../icon/icon.vue'
-
-import { X } from '@comz/icons'
-
-declare function emit(event: 'update:show', state: boolean): void
-declare function emit(event: 'on-close'): void
-
 declare const props: {
   title?: string
   width?: string
@@ -60,61 +38,33 @@ declare const props: {
   closable: boolean
 }
 
-export const handleModalClose = () => {
+declare function emit(event: 'update:show', state: boolean): void
+declare function emit(event: 'on-close'): void
+
+import { toRefs } from 'vue'
+import { useCssVars } from '@comz/vca'
+
+export { default as Card } from '../card/card.vue'
+export { default as Button } from '../button/button.vue'
+export { default as Icon } from '../icon/icon.vue'
+export { X } from '@comz/icons'
+
+const { closable, width } = toRefs(props)
+
+export const handleClose = () => {
   emit('update:show', false)
   emit('on-close')
 }
 
+export const handleMaskClick = () => {
+  if (!closable.value) return
+
+  handleClose()
+}
+
 export const cssVars = useCssVars({
-  '--c-modal-width': computed(() => props.width)
-})
+  '--cmodal-width': width
+}, { scoped: true })
 
-export default {
-  components: {
-    Card, Button, Icon, X
-  }
-}
+export default {}
 </script>
-
-<style lang="scss">
-$block: '.c-modal';
-
-%modal {
-  box-sizing: border-box;
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  height: 100%;
-  z-index: 600;
-}
-
-%modal-content {
-  position: relative;
-  top: 100px;
-  flex: none;
-  width: var(--c-modal-width, 300px);
-}
-
-
-#{$block} {
-  @extend %modal;
-
-  &__content { @extend %modal-content; }
-}
-
-.modal-fade-enter-active { animation: modal-fade-in .3s; }
-.modal-fade-leave-active { animation: modal-fade-out .3s; }
-
-@keyframes modal-fade-in {
-  0% { transform: translate3d(0, -20px, 0); opacity: 0; }
-  100% { transform: translate3d(0, 0, 0); opacity: 1; }
-}
-@keyframes modal-fade-out {
-  0% { transform: translate3d(0, 0, 0); opacity: 1; }
-  100% { transform: translate3d(0, -20px, 0); opacity: 0; }
-}
-</style>
