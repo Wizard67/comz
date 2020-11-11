b55abfee6d526a982bc9efc35c2a05d81ed66d1e
Wizard67 <wizard67.hjn@gmail.com>
2020-11-11 18:27:25 +0800

    refactor(component:card): refactor component

--- packages/comz/src/card/card.vue
+++ packages/comz/src/card/card.vue
@@ -1,77 +1,40 @@
 <template>
-  <section :class="cardClassName">
-    <div v-if="$slots.header" class="c-card__header">
-      <slot name="header"></slot>
+  <section
+    class="ccard"
+    :class="className"
+  >
+    <div
+      v-if="$slots.header"
+      class="ccard__header"
+    >
+      <slot name="header" />
     </div>
-    <div class="c-card__body">
-      <slot></slot>
-    </div>
-    <div v-if="$slots.footer" class="c-card__footer">
-      <slot name="footer"></slot>
+
+    <div class="ccard__body"><slot /></div>
+
+    <div
+      v-if="$slots.footer"
+      class="ccard__footer"
+    >
+      <slot name="footer" />
     </div>
   </section>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed } from 'vue'
-import { useClassName } from '@comz/vca'
-
 declare const props: {
   shadow: boolean
 }
 
-export const cardClassName = useClassName('c-card', {
-  'shadow': computed(() => props.shadow)
-})
+import { ref, toRefs } from 'vue'
+import { useBEM } from '@comz/vca'
+
+const { shadow } = toRefs(props)
+
+export const className = useBEM(({b, e, m}) => ({
+  [b('ccard')]: ref(true),
+  [m('shadow')]: shadow
+}), { blockPrefix: false })
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.c-card';
-
-%card {
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: auto;
-  background-color: white;
-  font-size: 14px;
-}
-%card-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 8px 16px;
-  border-bottom: 1px rgba(0, 0, 0, .08) solid;
-}
-%card-body {
-  padding: 16px 16px;
-}
-%card-footer {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  padding: 8px 16px;
-  border-top: 1px rgba(0, 0, 0, .08) solid;
-}
-
-#{$block} {
-  @extend %card;
-  border: 1px rgba(0, 0, 0, .08) solid;
-
-  &__header { @extend %card-header; }
-  &__body { @extend %card-body; }
-  &__footer { @extend %card-footer; }
-}
-
-#{$block}--shadow {
-  @extend %card;
-  box-shadow: 1px 1px 4px 0 rgba(0, 0, 0, .2);
-
-  &__header { @extend %card-header; }
-  &__body { @extend %card-body; }
-  &__footer { @extend %card-footer; }
-}
-</style>
