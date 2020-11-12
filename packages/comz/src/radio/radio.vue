eeca687aa0343cd39bdbcb64777cee211b194e84
Wizard67 <wizard67.hjn@gmail.com>
2020-11-12 15:58:49 +0800

    refactor(component:radio): refactor component

--- packages/comz/src/radio/radio.vue
+++ packages/comz/src/radio/radio.vue
@@ -1,5 +1,5 @@
 <template>
-  <label :class="cradio">
+  <label class="cradio" :class="className">
     <input
       class="cradio__field"
       type="radio"
@@ -13,9 +13,6 @@
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed, toRefs } from 'vue'
-import { useClassName } from '@comz/vca'
-
 declare const props: {
   value: string
   modelValue: string | number
@@ -24,13 +21,18 @@ declare const props: {
 
 declare function emit (event: 'update:modelValue', value: any): void
 
-export const { disabled } = toRefs(props)
+import { computed, ref, toRefs } from 'vue'
+import { useBEM } from '@comz/vca'
+
+const { disabled } = toRefs(props)
+
 export const checked = computed(() => props.value === props.modelValue)
 
-export const cradio = useClassName('cradio', {
-  'checked': checked,
-  'disabled': disabled
-})
+export const className = useBEM(({b, e, m}) => ({
+  [b('cradio')]: ref(true),
+  [m('checked')]: checked,
+  [m('disabled')]: disabled
+}), { blockPrefix: false })
 
 export const handleValueChange = (event: InputEvent) => {
   emit('update:modelValue', (event.target as HTMLInputElement).value)
@@ -38,93 +40,3 @@ export const handleValueChange = (event: InputEvent) => {
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.cradio';
-
-%radio {
-  position: relative;
-  box-sizing: border-box;
-  display: inline-flex;
-  align-items: center;
-  padding: 4px 0;
-  color: rgb(51, 51, 51);
-  font-size: 14px;
-  line-height: 1.2em;
-  cursor: pointer;
-
-  &:focus {
-    outline: none;
-  }
-
-  &::before,
-  &::after {
-    content: "";
-    box-sizing: border-box;
-    position: absolute;
-    top: 50%;
-    left: 0;
-    width: 1em;
-    height: 1em;
-    border-radius: 50%;
-  }
-  &::before {
-    transform: translateY(-50%) translateZ(0);
-    border: 2px rgb(52, 142, 199) solid;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-    cursor: inherit;
-  }
-
-  &::after {
-    transform: translateY(-50%) translateZ(0) scale(.1);
-    transform-origin: center;
-    background-color: rgb(52, 142, 199);
-    opacity: 0;
-    transition: background-color .2s ease-in-out,
-                transform .15s ease-in-out,
-                opacity .1s ease-in-out;
-  }
-}
-
-#{$block} {
-  @extend %radio;
-
-  &--checked {
-    @extend %radio;
-
-    &::after {
-      transform: translateY(-50%) translateZ(0) scale(.52);
-      opacity: 1;
-    }
-  }
-
-  &--disabled {
-    @extend %radio;
-
-    color: rgba(0, 0, 0, .3);
-    cursor: not-allowed;
-
-    &::before {
-      border: 2px rgba(0, 0, 0, .2) solid;
-      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    }
-    &::after {
-      background-color: rgba(0, 0, 0, .2);
-    }
-  }
-
-  &--checked--disabled {
-    @extend #{$block}--checked; 
-    @extend #{$block}--disabled; 
-  }
-}
-
-#{$block}__field {
-  width: 1em;
-  height: 1em;
-  margin: 0 .25em 0 0;
-  opacity: 0;
-  font-size: inherit;
-  cursor: inherit;
-}
-</style>
