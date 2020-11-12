e3d94d00eb86d508332e8237a36042f6b7ac42df
Wizard67 <wizard67.hjn@gmail.com>
2020-11-12 14:39:22 +0800

    refactor(component:input): refactor component

--- packages/comz/src/input/input.vue
+++ packages/comz/src/input/input.vue
@@ -1,11 +1,9 @@
 <template>
-  <div
-    class="cinput"
-    :style="inputCssVars"
-  >
+  <div class="cinput" :style="inputCssVars">
     <input
       type="text"
-      :class="cinput__field"
+      class="cinput__field"
+      :class="className"
       :style="inputFieldCssVars"
       :value="modelValue"
       :placeholder="placeholder"
@@ -14,18 +12,12 @@
       @input="handleInput"
     >
     <div v-if="showClearButton" class="cinput__clear">
-      <Icon @on-click="handleClear" link><X/></Icon>
+      <Icon @on-click="handleClear" link><X /></Icon>
     </div>
   </div>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed, toRefs } from 'vue'
-import { useClassName, useCssVars } from '@comz/vca'
-
-export { default as Icon } from '../icon/icon.vue'
-export { X } from '@comz/icons'
-
 declare const props: {
   modelValue: string
   placeholder?: string
@@ -37,11 +29,19 @@ declare const props: {
 
 declare function emit (event: 'update:modelValue', value: any): void
 
+import { computed, ref, toRefs } from 'vue'
+import { useBEM, useCssVars } from '@comz/vca'
+
+export { default as Icon } from '../icon/icon.vue'
+export { X } from '@comz/icons'
+
 const { disabled, width } = toRefs(props)
 
-export const cinput__field = useClassName('cinput__field', {
-  'disabled': disabled
-})
+export const className = useBEM(({ b, e, m }) => ({
+  [b('cinput')]: ref(true),
+  [e('field')]: ref(true),
+  [m('disabled')]: disabled
+}), { blockPrefix: false })
 
 export const inputCssVars = useCssVars({
   '--cinput-width': width
@@ -51,7 +51,7 @@ export const inputFieldCssVars = useCssVars({
   '--cinput-field-rpadding': computed(() => 
     showClearButton.value ? '1.5em': undefined
   )
-}, { scoped: true })
+})
 
 export const showClearButton = computed(() =>
   props.modelValue?.length > 0 &&
@@ -70,64 +70,3 @@ export const handleClear = () => {
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.cinput';
-
-#{$block} {
-  position: relative;
-  display: inline-flex;
-  justify-content: space-between;
-  align-items: center;
-  width: var(--cinput-width, 100%);
-  border-radius: 2px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-  overflow: hidden;
-}
-
-%input-field {
-  box-sizing: border-box;
-  flex: 1;
-  position: relative;
-  width: 160px;
-  max-width: 100%;
-  height: 100%;
-  padding: 4px 8px;
-  padding-right: var(--cinput-field-rpadding, 8px);
-  border: none;
-  color: rgb(51, 51, 51);
-  font-size: 14px;
-  line-height: 1.2em;
-
-  &:focus {
-    outline: none;
-  }
-
-  &:read-only {
-    color: rgba(0, 0, 0, .3);
-  }
-}
-
-#{$block}__field {
-  @extend %input-field;
-}
-
-#{$block}__field--disabled {
-  @extend %input-field;
-  color: rgba(0, 0, 0, .2);
-  background-color: rgb(235, 235, 235);
-  cursor: not-allowed;
-}
-
-#{$block}__clear {
-  flex: none;
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translateY(-50%);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 1.5em;
-}
-</style>
