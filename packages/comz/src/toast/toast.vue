876fb645b079d905f68024cbf423d23e0d9b7418
Wizard67 <wizard67.hjn@gmail.com>
2020-11-12 14:17:05 +0800

    refactor(component:toast): refactor component

--- packages/comz/src/toast/toast.vue
+++ packages/comz/src/toast/toast.vue
@@ -1,6 +1,6 @@
 <template>
   <teleport to="body">
-    <transition name="ctoast-fade">
+    <transition name="ctoast">
       <div
         v-if="show"
         class="ctoast"
@@ -13,15 +13,15 @@
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { computed, toRefs } from 'vue'
-import { useOverlay, useCssVars } from '@comz/vca'
-
 declare const props: {
   show: boolean
   duration?: number
 }
 
 declare function emit(event: 'update:show', state: boolean): void
+
+import { computed, toRefs } from 'vue'
+import { useOverlay, useCssVars } from '@comz/vca'
 
 const { show, duration } = toRefs(props)
 
@@ -40,55 +40,3 @@ export const cssVars = useCssVars({
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.ctoast';
-
-#{$block} {
-  box-sizing: border-box;
-  position: fixed;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, var(--ctoast-top));
-  display: flex;
-  min-width: 200px;
-  max-width: 500px;
-  padding: 8px 16px;
-  border-radius: 4px;
-  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
-  color: rgb(51, 51, 51);
-  background-color: white;
-  font-size: 14px;
-  line-height: 1.2em;
-  transition: transform .3s ease-in-out;
-}
-
-.ctoast-fade-enter-active {
-  animation: ctoast-fade-in .3s ease-in-out;
-}
-.ctoast-fade-leave-active {
-  animation: ctoast-fade-out .3s ease-in-out;
-}
-
-@keyframes ctoast-fade-in {
-  0% {
-    transform: translate3d(-50%, -100%, 0);
-    opacity: 0;
-  }
-  100% {
-    transform: translate3d(-50%, var(--ctoast-top), 0);
-    opacity: 1;
-  }
-}
-
-@keyframes ctoast-fade-out {
-  0% {
-    transform: translate3d(-50%, var(--ctoast-top), 0);
-    opacity: 1;
-  }
-  100% {
-    transform: translate3d(-50%, -100%, 0);
-    opacity: 0;
-  }
-}
-</style>
