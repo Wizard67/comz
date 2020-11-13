87c1a0c13cbc14f58d41aa5954f004f50ce19045
Wizard67 <wizard67.hjn@gmail.com>
2020-11-13 17:05:39 +0800

    refactor(component:switch): refactor component

--- packages/comz/src/switch/switch.vue
+++ packages/comz/src/switch/switch.vue
@@ -1,15 +1,13 @@
 <template>
   <div
-    :class="cswitch"
+    class="cswitch"
+    :class="className"
     @click="handleValueChange"
   >
   </div>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { toRefs } from 'vue'
-import { useClassName } from '@comz/vca'
-
 declare const props: {
   modelValue: boolean
   disabled: boolean
@@ -17,12 +15,16 @@ declare const props: {
 
 declare function emit (event: 'update:modelValue', value: boolean): void
 
+import { ref, toRefs } from 'vue'
+import { useBEM } from '@comz/vca'
+
 export const { modelValue, disabled } = toRefs(props)
 
-export const cswitch = useClassName('cswitch', {
-  'checked': modelValue,
-  'disabled': disabled
-})
+export const className = useBEM(({b, m}) => ({
+  [b('cswitch')]: ref(true),
+  [m('checked')]: modelValue,
+  [m('disabled')]: disabled
+}), { blockPrefix: false })
 
 export const handleValueChange = () => {
   if (disabled.value) return
@@ -31,84 +33,3 @@ export const handleValueChange = () => {
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.cswitch';
-
-%switch {
-  position: relative;
-  box-sizing: border-box;
-  display: inline-flex;
-  align-items: center;
-  width: 2.5em;
-  height: 22px;
-  padding: 4px 0;
-  color: rgb(52, 142, 199);
-  font-size: 14px;
-  line-height: 1.2em;
-  cursor: pointer;
-
-  &::before,
-  &::after {
-    content: "";
-    box-sizing: border-box;
-    position: absolute;
-    top: 50%;
-    left: 0;
-  }
-  &::before {
-    left: .5em;
-    transform: translateY(-50%);
-    width: 1.5em;
-    height: 4px;
-    border-radius: 200px;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-    background-color: currentColor;
-    clip-path: polygon(.5em 0, 100% 0, 100% 100%, .5em 100%);
-    transition: clip-path 0s linear 0.02s;
-  }
-
-  &::after {
-    width: 1em;
-    height: 1em;
-    border-radius: 50%;
-    transform: translateY(-50%) translateZ(0);
-    border: 2px currentColor solid;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-    transition: transform .1s ease-in-out,
-                background-color .1s ease-in-out;
-    cursor: inherit;
-  }
-}
-
-#{$block} {
-  @extend %switch;
-
-  &--checked {
-    @extend %switch;
-
-    &::before {
-      clip-path: none;
-      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-      transform: translateY(-50%) translateZ(0);
-    }
-
-    &::after {
-      background-color: currentColor;
-      transform: translateX(1.5em) translateY(-50%) translateZ(0);
-    }
-  }
-
-  &--disabled {
-    @extend %switch;
-
-    color: #c7c7c7;
-    cursor: not-allowed;
-  }
-
-  &--checked--disabled {
-    @extend #{$block}--checked; 
-    @extend #{$block}--disabled; 
-  }
-}
-</style>
