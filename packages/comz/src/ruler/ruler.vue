5f82333d4adc8f6fa9d328135debce545d254950
Wizard67 <wizard67.hjn@gmail.com>
2020-11-13 17:31:31 +0800

    refactor(component:ruler): refactor component

--- packages/comz/src/ruler/ruler.vue
+++ packages/comz/src/ruler/ruler.vue
@@ -1,26 +1,28 @@
 <template>
   <section
     ref="rulerRef"
-    :class="cruler"
+    class="cruler"
+    :class="className"
   >
   </section>
 </template>
 
 <script setup="props, { emit }" lang="ts">
-import { toRefs } from 'vue'
-import { useClassName, useCssVars } from '@comz/vca'
-
 declare const props: {
   backgroundColor?: string
   borderColor?: string
   grid: boolean
 }
 
+import { ref, toRefs } from 'vue'
+import { useBEM, useCssVars } from '@comz/vca'
+
 export const { grid, backgroundColor, borderColor } = toRefs(props)
 
-export const cruler = useClassName('cruler', {
-  'grid': grid
-})
+export const className = useBEM(({b, m}) => ({
+  [b('cruler')]: ref(true),
+  [m('grid')]: grid
+}), { blockPrefix: false })
 
 export const cssVars = useCssVars({
   '--cruler-background-color': backgroundColor,
@@ -29,26 +31,3 @@ export const cssVars = useCssVars({
 
 export default {}
 </script>
-
-<style lang="scss">
-$block: '.cruler';
-
-
-#{$block} {
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-
-  --border-clor: var(--cruler-border-color, rgb(247, 247, 247));
-
-  &--grid {
-    @extend #{$block};
-    background: var(--cruler-background-color, white);
-    background-image: linear-gradient(var(--border-clor) 1px, transparent 0),
-                      linear-gradient(90deg, var(--border-clor) 1px, transparent 0),
-                      linear-gradient(var(--border-clor) 1px, transparent 0),
-                      linear-gradient(90deg, var(--border-clor) 1px, transparent 0);
-    background-size: 10px 10px, 10px 10px, 70px 70px, 70px 70px;
-  }
-}
-</style>
