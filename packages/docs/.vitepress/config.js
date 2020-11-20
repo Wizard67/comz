557cf91308a9483989e3d535491e2c89affc5db7
Wizard67 <wizard67.hjn@gmail.com>
2020-11-20 17:11:26 +0800

    feat: initialize docs

---
+++ packages/docs/.vitepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: 'COMZ',
+  description: 'A UI components base on Vue3.',
+
+  themeConfig: {
+    repo: 'Wizard67/comz',
+    docsDir: 'docs',
+    editLinkText: 'Edit this page on GitHub',
+
+    nav: [
+      // { text: '', link: ''}
+    ],
+
+    sidebar: {
+      '/components/': [
+        {
+          text: '组件',
+          children: [ ]
+        }
+      ]
+    }
+  }
+}
