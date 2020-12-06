e8a676c84364e43fd95154cc6590632a22b731c8
Wizard67 <wizard67.hjn@gmail.com>
2020-12-06 22:20:30 +0800

    test(component:flex): add test

---
+++ packages/comz/src/flex/__test__/flex.spec.ts
@@ -0,0 +1,98 @@
+import { mount } from '@vue/test-utils'
+import Flex from '../flex.vue'
+
+describe('Button.vue', () => {
+  describe('props', () => {
+    it('inline', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          inline: true
+        }
+      })
+
+      expect(wrapper.classes()).toContain('cflex--inline')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('gap', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          gap: '10px'
+        }
+      })
+
+      expect(wrapper.attributes('style')).toContain('--cflex-gap: 10px;')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('justify', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          justify: 'center'
+        }
+      })
+
+      expect(wrapper.attributes('style')).toContain('--cflex-justify: center;')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('align', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          align: 'flex-end'
+        }
+      })
+
+      expect(wrapper.attributes('style')).toContain('--cflex-align: flex-end;')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('center', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          center: true
+        }
+      })
+
+      expect(wrapper.classes()).toContain('cflex--center')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('vertical', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          vertical: true
+        }
+      })
+
+      expect(wrapper.classes()).toContain('cflex--vertical')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    it('fill', () => {
+      const wrapper = mount(Flex, {
+        props: {
+          fill: true
+        }
+      })
+
+      expect(wrapper.classes()).toContain('cflex--fill')
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+
+  describe('slots', () => {
+    it('default', () => {
+      const content = "flex"
+
+      const wrapper = mount(Flex, {
+        slots: {
+          default: content
+        }
+      })
+
+      expect(wrapper.text()).toContain(content)
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
