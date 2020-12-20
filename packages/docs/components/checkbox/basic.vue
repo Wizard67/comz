89109d78f14b8bed70aa13838a6270f40b118575
Wizard67 <wizard67.hjn@gmail.com>
2020-12-20 16:00:09 +0800

    docs: fix demo display

--- packages/docs/components/checkbox/basic.vue
+++ packages/docs/components/checkbox/basic.vue
@@ -1,10 +1,10 @@
 <template>
   <Flex gap="160px">
-    <Flex gap="24px">
+    <Flex gap="24px" inline>
       <Checkbox v-model="value" value="A">A</Checkbox>
       <Checkbox v-model="value" value="B">B</Checkbox>
     </Flex>
-    <Flex gap="24px">
+    <Flex gap="24px" inline>
       <Checkbox v-model="value" value="A" disabled>A</Checkbox>
       <Checkbox v-model="value" value="B" disabled>B</Checkbox>
     </Flex>
