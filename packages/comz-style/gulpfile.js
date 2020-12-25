e71a4861eb7d41880cbb75a2849d2a550633371f
Wizard67 <wizard67.hjn@gmail.com>
2020-12-25 17:13:19 +0800

    build: style build with cssnano

--- packages/comz-style/gulpfile.js
+++ packages/comz-style/gulpfile.js
@@ -2,12 +2,16 @@ const { src, dest } = require('gulp')
 const postcss = require('gulp-postcss')
 const sass = require('gulp-dart-sass')
 const flexGapPolyfill = require('flex-gap-polyfill')
+const cssnano = require('cssnano')
 
 exports.build = () => (
   src('./*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(postcss([
-      flexGapPolyfill()
+      flexGapPolyfill(),
+      cssnano({
+        preset: 'default'
+      })
     ]))
     .pipe(dest('./dist/') )
 )
