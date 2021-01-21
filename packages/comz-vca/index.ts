adbcd60fdeb89afb7d26d07961fe19fb89895fd5
Wizard67 <wizard67.hjn@gmail.com>
2021-01-21 18:45:14 +0800

    refactor: deprecate and remove useHeightToggle

--- packages/comz-vca/index.ts
+++ packages/comz-vca/index.ts
@@ -4,7 +4,6 @@ export * from './src/useToggle'
 export * from './src/useBEM'
 export * from './src/useCssVars'
 export * from './src/useOverlay'
-export * from './src/useHeightToggle'
 export * from './src/useMouse'
 
 export * from './src/usePopper'
