8bf2de466a6b1860314a4a1e3420a41994dff533
Wizard67 <wizard67.hjn@gmail.com>
2021-04-20 19:06:40 +0800

    build(docs): disable clean-css minify

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,9 +1,17 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   optimizeDeps: {
     include: [
       // 'comz',
       '@comz/icons',
       '@comz/vca'
     ]
+  },
+  build: {
+    cleanCssOptions: {
+      format: 'beautify',
+      level: 0
+    }
   }
-}
+})
