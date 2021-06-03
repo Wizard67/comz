7be0a643b3ad0de8f2b6139250c31c03b4dda189
Wizard67 <wizard67.hjn@gmail.com>
2021-06-03 18:09:30 +0800

    feat(icons): add more icons

---
+++ packages/comz-icons/src/dash-lg.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M0 8a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1z"/>
+  </svg>
+</template>
---
+++ packages/comz-icons/src/slash-lg.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M14.707 1.293a1 1 0 0 1 0 1.414l-12 12a1 1 0 0 1-1.414-1.414l12-12a1 1 0 0 1 1.414 0z"/>
+  </svg>
+</template>
